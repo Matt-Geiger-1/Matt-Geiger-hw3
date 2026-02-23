697aac06b55a2f0058fe3fa580f55525f9a8fce0
Matt-Geiger-1 <matt.geiger@kellogg.northwestern.edu>
2026-02-23 03:22:42 +0000

    Staging everything at once

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Root page
+  get "/", { :controller => "places", :action => "index" }
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  # Places
+  get "/places", { :controller => "places", :action => "index" }
+  get "/places/new", { :controller => "places", :action => "new" }
+  post "/places", { :controller => "places", :action => "create" }
+  get "/places/:id", { :controller => "places", :action => "show" }
+
+  # Entries
+  get "/entries/new", { :controller => "entries", :action => "new" }
+  post "/entries", { :controller => "entries", :action => "create" }
+
 end
